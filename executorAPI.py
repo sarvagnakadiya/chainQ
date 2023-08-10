3f42c3286da9637549865832807ffceb290aa662
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-08-10 15:21:25 +0530

    executor code changes

--- executorAPI.py
+++ executorAPI.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, jsonify
-from agent_executor import get_answer
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to the Answer Retrieval API! Send a POST request to /get_answer with a JSON body containing 'user_prompt' to get an answer."
 
 @app.route('/get_answer', methods=['POST'])
 def get_answer_route():
     try:
         user_prompt = request.json['user_prompt']  # Assuming the input is sent as JSON in the request body
         
+        # You would need to import and implement the 'get_answer' function
         answer = get_answer(user_prompt)
         return jsonify({'answer': answer})
     except Exception as e:
