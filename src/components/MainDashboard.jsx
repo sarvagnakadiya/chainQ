e8ed2784c087a1146c56a4e07752b94c0e209fc3
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-08-12 12:14:19 +0530

    packages removed and styles added

--- src/components/MainDashboard.jsx
+++ src/components/MainDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "../style/main.scss";
-import EmptyComponent from "./EmptyComponent";
+// import CovalentAPIs from "./EmptyComponent";
 import CovalentAPIs from "./CovalentAPIs";
 import ChatLog from "./ChatLog";
 import MessageHistory from "./MessageHistory";
