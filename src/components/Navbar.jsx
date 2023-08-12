3af56117cd21996548529ee92a0a898cb5e88fd9
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-08-12 11:35:05 +0530

    rainbow removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import ConnectButtonCustom from "./Connectbuttoncustom";
-import "../style/ConnectButtonCustom.scss";
+// import ConnectButtonCustom from "./Connectbuttoncustom";
+// import "../style/ConnectButtonCustom.scss";
 import logo from "../assets/logo.png";
 
 function Navbar() {
@@ -20,9 +20,7 @@ function Navbar() {
               <img src={logo} alt="logo" style={{ width: "30%" }} />
             </Link>
           </span>
-          <div style={{ width: "30%" }}>
-            <ConnectButtonCustom />
-          </div>
+          <div style={{ width: "30%" }}>{/* <ConnectButtonCustom /> */}</div>
         </nav>
       </header>
     </>
--- src/index.js
+++ src/index.js
@@ -3,36 +3,35 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+// import "@rainbow-me/rainbowkit/styles.css";
+// import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+// import { configureChains, createConfig, WagmiConfig } from "wagmi";
+// import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
+// import { alchemyProvider } from "wagmi/providers/alchemy";
+// import { publicProvider } from "wagmi/providers/public";
 
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, zora],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
-);
-const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
-  chains,
-});
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
+// const { chains, publicClient } = configureChains(
+//   [mainnet, polygon, optimism, arbitrum, zora],
+//   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+// );
+// const { connectors } = getDefaultWallets({
+//   appName: "My RainbowKit App",
+//   projectId: "YOUR_PROJECT_ID",
+//   chains,
+// });
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors,
+//   publicClient,
+// });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <WagmiConfig config={wagmiConfig}>
-    <RainbowKitProvider chains={chains}>
-      {" "}
-      <App />
-    </RainbowKitProvider>
-  </WagmiConfig>
+  // <WagmiConfig config={wagmiConfig}>
+  //   <RainbowKitProvider chains={chains}>
+  <App />
+  //   </RainbowKitProvider>
+  // </WagmiConfig>
 );
 
 // If you want to start measuring performance in your app, pass a function
